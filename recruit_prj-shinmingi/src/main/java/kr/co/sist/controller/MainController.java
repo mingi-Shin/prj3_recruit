3d4bab1b8920c3c8e5663967972ad3fef82ed68f
mingi-Shin <shinseogyong0621@gmail.com>
2025-09-13 17:28:55 +0900

    0913

--- recruit_prj-shinmingi/src/main/java/kr/co/sist/controller/MainController.java
+++ recruit_prj-shinmingi/src/main/java/kr/co/sist/controller/MainController.java
@@ -1,9 +1,13 @@
 package kr.co.sist.controller;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.co.sist.jwt.AuthController;
@@ -39,18 +43,46 @@ public class MainController {
 	}
 	
 	@GetMapping("/corp/main")
-	public String corpMainPage(HttpServletRequest request, @AuthenticationPrincipal CustomUser user, Model model) {
+	@ResponseBody
+	public Map<String, Object> corpMainPage(HttpServletRequest request, @AuthenticationPrincipal CustomUser user, Model model) {
 		// @AuthenticationPrincipal : Spring Security가 SecurityContextHolder에서 현재 인증(Authentication) 정보를 꺼내서 주입해주는 어노테이션, 현재 없음 
 		
 		CustomUser loginUser = null;
-		String accessToken = request.getHeader("accessToken");
-		System.out.println("@AuthenticationPrincipal 테스트중 " + user);
-		System.out.println("accessToken 테스트중 " + accessToken);
+		String Authorization = request.getHeader("Authorization");
+		System.out.println("@AuthenticationPrincipal 테스트중 = " + user); // 나옴 
+		System.out.println("Authorization 테스트중 = " + Authorization); // 나옴 
 		
-		//테스트
 		
-		return "corp/main_page";
+		//json으로 응답
+    Map<String, Object> response = new HashMap<>();
+    response.put("redirectUrl", "/corp/main_page");
+    response.put("user", user);
+		
+		return response;
 	}
+	
+	@GetMapping("/corp/main_page")
+	public String corpMainPage2(HttpServletRequest request, @AuthenticationPrincipal CustomUser user, Model model) {
+		// @AuthenticationPrincipal : Spring Security가 SecurityContextHolder에서 현재 인증(Authentication) 정보를 꺼내서 주입해주는 어노테이션, 현재 없음 
+		
+		System.out.println("/corp/main_page 가는 중----------------");
+	
+		String Authorization = request.getHeader("Authorization");
+		System.out.println("@AuthenticationPrincipal 테스트중 = " + user); // 나옴 
+		System.out.println("Authorization 테스트중 = " + Authorization); // 나옴
+		
+		System.out.println("/corp/main_page 가는 끝 ----------------");
+		
+		/**
+		 * access를 param으로 받아서 SecurityContextHolder에 넣는건..?
+		 * 	•	URL 파라미터로 accessToken을 전달하면 브라우저 히스토리, 서버 로그, 프록시 로그 등 어디서든 토큰이 평문으로 노출됩니다.
+				•	그러면 JWT를 localStorage에 넣고 헤더로만 쓰는 의미가 사라지고, 보안상 거의 무용지물이 돼요.
+		 */
+		return "/corp/main_page";
+	}
+	
+	
+	
 	
 	
 	
